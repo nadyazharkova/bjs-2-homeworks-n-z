a37699405256512cd8442f46b858f535a2dd38fc
N.Zharkova <nadezhda.a.zharkova@gmail.com>
2023-12-03 17:55:18 +0500

    firstHomework

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
 "use strict"
 function solveEquation(a, b, c) {
+  let discriminant = b ** 2 - 4 * a * c;
+  let first_value;
+  let second_value;
   let arr = [];
-  
-  return arr;
+  if (discriminant > 0) {
+    first_value = (-b + Math.sqrt(discriminant)) / (2 * a);
+    second_value = (-b - Math.sqrt(discriminant)) / (2 * a);
+    arr = [first_value, second_value];
+    return arr;
+  } else if (discriminant == 0) {
+    first_value = (-b + Math.sqrt(discriminant)) / (2 * a);
+    arr = [first_value];
+    return arr;
+  } else {
+    return arr;
+  }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  percent = percent / 100 / 12;
+  amount = amount - contribution;
+  let monthlyPayment = amount * (percent + (percent / (((1 + percent)**countMonths) - 1)));
+  let allAmount = monthlyPayment * countMonths;
+  allAmount = allAmount.toFixed(2);
+  allAmount = Number(allAmount);
+  return allAmount;
 }
