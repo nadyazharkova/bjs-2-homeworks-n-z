4275358150173936aebde32301176429d0ba7408
N.Zharkova <nadezhda.a.zharkova@gmail.com>
2023-12-18 23:04:55 +0500

    objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,26 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if (this.marks) {
+        this.marks.push(...marksToAdd);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    return this.marks?.reduce((sum, number) => sum + number / this.marks.length, 0) || 0;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
 }
